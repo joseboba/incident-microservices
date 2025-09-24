88812c2a8e5eed0a5c3172f876b0390197d426ba
Jose Enrique Bobadilla <joseboba18@gmail.com>
2025-09-23 22:31:31 -0600

    priority level

--- src/modules/incident/queries/handlers/get-incident-list.handler.ts
+++ src/modules/incident/queries/handlers/get-incident-list.handler.ts
@@ -41,6 +41,9 @@ export class GetIncidentListHandler
           technicianUserAppId: user.isTechnical ? userApp.userAppId : undefined,
         },
       },
+      relations: {
+        incidentPriorityLevel: true,
+      }
     });
   }
 
